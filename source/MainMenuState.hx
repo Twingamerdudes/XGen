ae436db3f4eca2eaf20dcaa07842c894a8aa9c92
Twingamerdudes <81382687+Twingamerdudes@users.noreply.github.com>
2021-12-16 20:38:57 -0500

    Updated version

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -34,7 +34,7 @@ class MainMenuState extends MusicBeatState
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 
-	public static var xGenVersion:String = "0.0.1";
+	public static var xGenVersion:String = "0.0.2";
 
 	override function create()
 	{
