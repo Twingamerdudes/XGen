6a16c58916c96a6178c98f9c5bae2edee19436b9
Twingamerdudes <81382687+Twingamerdudes@users.noreply.github.com>
2022-02-03 19:09:05 -0500

    Looks like I forgot to update versionText

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1723,7 +1723,7 @@ class PlayState extends MusicBeatState
 		}
 		var timeList:Array<String>;
 		//timerTxt.text = /*Std.string(Date.fromTime(songLength)) */ time;
-		songNameTxt.text = "Playing: " + SONG.song + " on XGen 0.0.6";
+		songNameTxt.text = "Playing: " + SONG.song + " on XGen 0.0.7";
 		accurayTxt.text = "Accuracy: %" + songAccuracy;
 		comboBreaksTxt.text = "Combo breaks: " + songComboBreaks;
 
